9d941b3062f0413b17b0ae9450426604e8e604cd
Kuba <kuba.slawinski.2003@gmail.com>
2024-09-18 16:41:13 +0200

    Added MediaFile.java

--- src/main/java/com/example/Memories/model/MediaFile.java
+++ src/main/java/com/example/Memories/model/MediaFile.java
@@ -1,2 +1,19 @@
-package com.example.Memories.model;public class MediaFile {
+package com.example.Memories.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MediaFile {
+    @Id
+    private Long id;
+    private String mediaLink;
 }
